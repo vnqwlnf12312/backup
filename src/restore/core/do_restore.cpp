fc86c1d303d7fa305b782b4f114e2af4f2c884ef
amir <biktagirov.akh@phystech.edu>
2024-11-10 15:01:33 +0300

    upgraded restore

--- src/restore/core/do_restore.cpp
+++ src/restore/core/do_restore.cpp
@@ -1,6 +1,7 @@
 #include "do_restore.hpp"
 
 #include <format>
+#include <iostream>
 
 #include <boost/filesystem.hpp>
 
@@ -16,10 +17,44 @@ namespace filesystem = boost::filesystem;
 void RestoreBackup(const filesystem::path& from_path,
                    const filesystem::path& to_path,
                    filesystem::copy_options copy_options) {
+  system::error_code error_code;
   for (const auto& direntry :
-       filesystem::recursive_directory_iterator(from_path)) {
+       filesystem::recursive_directory_iterator(from_path, error_code)) {
     const auto cur_path = direntry.path();
+    if (error_code.failed()) {
+      std::cout << std::format("failed to read {}. error message: {}",
+                               cur_path.string(), error_code.what());
+      continue;
+    }
     if (cur_path.filename() == utils::kBackupTag) {
+      continue;
+    }
+
+    if (cur_path.filename() == utils::kDeleteMark) {
+      const auto dir_path = cur_path.parent_path();
+
+      const auto relative_path = dir_path.lexically_relative(from_path);
+      const auto new_path = to_path / relative_path;
+      filesystem::remove_all(new_path, error_code);
+      if (error_code.failed()) {
+        std::cout << std::format(
+            "failed to delete directory {}. error message: {}",
+            new_path.string(), error_code.what());
+        continue;
+      }
+      continue;
+    }
+
+    if (cur_path.filename().string().starts_with(utils::kDeleteMark)) {
+      const auto relative_path = cur_path.lexically_relative(from_path);
+      const auto new_path = to_path / cur_path.filename().string().substr(
+                                          utils::kDeleteMark.size());
+      filesystem::remove(new_path, error_code);
+      if (error_code.failed()) {
+        std::cout << std::format("failed to delete file {}. error message: {}",
+                                 new_path.string(), error_code.what());
+        continue;
+      }
       continue;
     }
 
@@ -27,7 +62,13 @@ void RestoreBackup(const filesystem::path& from_path,
 
     filesystem::path new_path = to_path / relative_path;
 
-    filesystem::copy(cur_path, new_path, copy_options);
+    filesystem::copy(cur_path, new_path, copy_options, error_code);
+    if (error_code.failed()) {
+      std::cout << std::format("failed to copy {} to {}. error message: {}",
+                               cur_path.string(), new_path.string(),
+                               error_code.what());
+      continue;
+    }
   }
 }
 
@@ -35,8 +76,16 @@ filesystem::path FindLastFullBackup(const filesystem::path& backup_path) {
   const auto backups_folder = backup_path.parent_path();
   filesystem::path latest_backup;
 
-  for (const auto& dirent : filesystem::directory_iterator(backups_folder)) {
+  system::error_code error_code;
+
+  for (const auto& dirent :
+       filesystem::directory_iterator(backups_folder, error_code)) {
     const auto cur_path = dirent.path();
+    if (error_code.failed()) {
+      std::cout << std::format("failed to read {}. error message: {}",
+                               cur_path.string(), error_code.what());
+      continue;
+    }
     if (cur_path.string() < backup_path.string() &&
         latest_backup.string() < cur_path.string() &&
         utils::GetBackupTag(cur_path) == utils::kFullTag) {
@@ -50,20 +99,48 @@ filesystem::path FindLastFullBackup(const filesystem::path& backup_path) {
 }  // namespace
 
 void DoRestore(const std::string& restore_from, const std::string& restore_to) {
+  system::error_code error_code;
+
   filesystem::path from_path = restore_from;
-  if (!filesystem::exists(from_path) || !filesystem::is_directory(from_path)) {
+
+  bool from_exists = filesystem::exists(from_path, error_code);
+  if (error_code.failed() &&
+      error_code != system::errc::no_such_file_or_directory) {
+    std::cout << std::format(
+        "failed to check {} for existance. error message: {}",
+        from_path.string(), error_code.what());
+    return;
+  }
+
+  bool from_is_directory = filesystem::is_directory(from_path, error_code);
+  if (error_code.failed()) {
+    std::cout << std::format(
+        "failed to check if {} is directory. error message: {}",
+        from_path.string(), error_code.what());
+    return;
+  }
+
+  if (!from_exists || !from_is_directory) {
     throw std::runtime_error(std::format(
         "can't restore from path {} doesn't exist or is not directory\n",
         from_path.string()));
   }
-  from_path = filesystem::canonical(from_path);
+  from_path = filesystem::canonical(from_path, error_code);
+  if (error_code.failed()) {
+    std::cout << std::format("failed to canonize {}. error message: {}",
+                             from_path.string(), error_code.what());
+    return;
+  }
 
   filesystem::path to_path = filesystem::canonical(restore_to);
 
-  system::error_code error_code;
+  if (error_code.failed()) {
+    std::cout << std::format("failed to canonize {}. error message: {}",
+                             to_path.string(), error_code.what());
+    return;
+  }
 
   filesystem::create_directories(to_path, error_code);
-
   if (error_code.failed() && error_code != system::errc::file_exists) {
     throw std::runtime_error(
         std::format("can't create {} directory for backup. error message: {}\n",
@@ -76,9 +153,16 @@ void DoRestore(const std::string& restore_from, const std::string& restore_to) {
   }
 
   const auto last_full_backup_path = FindLastFullBackup(from_path);
+  if (last_full_backup_path.string().empty()) {
+    std::cout << std::format(
+        "failed to find previous full backup for given incrementak backup {}",
+        from_path.string());
+    return;
+  }
 
-  RestoreBackup(from_path, to_path, filesystem::copy_options::copy_symlinks);
   RestoreBackup(last_full_backup_path, to_path,
+                filesystem::copy_options::copy_symlinks);
+  RestoreBackup(from_path, to_path,
                 filesystem::copy_options::copy_symlinks |
                     filesystem::copy_options::skip_existing);
 }
